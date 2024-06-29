9ac7447c22e4f08aa0754f76e1a56bdad23335ed
rlexus90 <109463776+rlexus90@users.noreply.github.com>
2024-06-29 10:40:49 +0200

    refractor: change path

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
-  product: "https://svedox86v6.execute-api.eu-west-2.amazonaws.com",
+  product: "https://tcmidzosvh.execute-api.eu-west-2.amazonaws.com",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://w0x63gw06c.execute-api.eu-west-2.amazonaws.com/prod",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
