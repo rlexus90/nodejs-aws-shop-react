3bc600d4762035667fd4bf6a1844e0a26d5e7f6e
rlexus90 <109463776+rlexus90@users.noreply.github.com>
2024-08-01 16:44:31 +0200

    refractor: change API path to task-9

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,12 +1,12 @@
 const API_PATHS = {
   product: "https://tcmidzosvh.execute-api.eu-west-2.amazonaws.com",
   // order: "http://localhost:4000/api",
-  order: "https://e0cim6hgw3.execute-api.eu-west-2.amazonaws.com/prod/api",
-  // order: "http://rlexus90-cart-api-prod.eu-west-2.elasticbeanstalk.com/api",
+  // order: "https://e0cim6hgw3.execute-api.eu-west-2.amazonaws.com/prod/api",
+  order: "http://rlexus90-cart-api-prod.eu-west-2.elasticbeanstalk.com/api",
   import: "https://b0jcw93dz5.execute-api.eu-west-2.amazonaws.com/prod",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://e0cim6hgw3.execute-api.eu-west-2.amazonaws.com/prod/api",
-  // cart: "http://rlexus90-cart-api-prod.eu-west-2.elasticbeanstalk.com/api",
+  // cart: "https://e0cim6hgw3.execute-api.eu-west-2.amazonaws.com/prod/api",
+  cart: "http://rlexus90-cart-api-prod.eu-west-2.elasticbeanstalk.com/api",
   // cart: "http://localhost:4000/api",
 };
 
